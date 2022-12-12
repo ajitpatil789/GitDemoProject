abcf9726f5545e6faeb6b0471172a60acce67820
Ajit <Ajit@DESKTOP-I8COK54>
2022-12-12 19:08:05 +0530

    Commiting Test2 to branchx

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "no";
+		 if (hungry == "yes")
+		 {
+			 System.out.println("Eat Something.....");
+		 }
+		 else 
+		 {
+			 System.out.println("Do work........");
+		 }
+	}
+
+}
